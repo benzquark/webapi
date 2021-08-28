5c8e29ee3e10f0f818c0bdffc0ed69dc3ca3cd62
benzquark <benzquark@gmail.com>
2021-08-28 08:43:06 -0700

    add menubar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
+const styles = theme =>({
+
+})
 class App extends React.Component {
   constructor(prop) {
     super(prop);
   }
 
   render() {
+    const { classes } = this.props;
     return ( 
-      <div >Helloworld </div>
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">            
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
     )
   }
 }
-export default App;
+export default withStyles(styles)(App);
