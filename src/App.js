c85187f2462870a153aee1adf5c56d0dc315f608
benzquark <benzquark@gmail.com>
2021-08-28 10:08:47 -0700

    add chart&api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +8,8 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+import axios from 'axios';
 
 
 const styles = theme =>({
@@ -19,9 +22,64 @@ const styles = theme =>({
     color: theme.palette.text.secondary,
   },
 })
+const data = [
+  {
+    name: 'Page A',
+    ยืนยันติดเชื้อ: 4000,
+    ยืนยันเสียชีวิต: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    ยืนยันติดเชื้อ: 3000,
+    ยืนยันเสียชีวิต: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    ยืนยันติดเชื้อ: 2000,
+    ยืนยันเสียชีวิต: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    ยืนยันติดเชื้อ: 2780,
+    ยืนยันเสียชีวิต: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    ยืนยันติดเชื้อ: 1890,
+    ยืนยันเสียชีวิต: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    ยืนยันติดเชื้อ: 2390,
+    ยืนยันเสียชีวิต: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    ยืนยันติดเชื้อ: 3490,
+    ยืนยันเสียชีวิต: 4300,
+    amt: 2100,
+  },
+];
+
 class App extends React.Component {
   constructor(prop) {
     super(prop);
+  }
+  componentDidMount(){
+    this.callAPI();
+  }
+
+  callAPI(){
+    axios.get('https://covid19.th-stat.com/json/covid19v2/getTimeline.json')
+    .then(Response => {
+      console.log(response.data)
+    })
   }
 
   render() {
@@ -37,29 +95,69 @@ class App extends React.Component {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Container>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography>Confirmed</Typography>
+            <Typography variant="h3">1000</Typography>
+            <Typography variant="h5">1000</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography>Hospitalized</Typography>
+            <Typography variant="h3">1000</Typography>
+            <Typography variant="h5">1000</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography>Recovered</Typography>
+            <Typography variant="h3">1000</Typography>
+            <Typography variant="h5">1000</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography>Death</Typography>
+            <Typography variant="h3">1000</Typography>
+            <Typography variant="h5">1000</Typography>
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          <Paper className={classes.paper}>
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="ยืนยันติดเชื้อ" fill="#FFC300" />
+                <Bar dataKey="ยืนยันเสียชีวิต" fill="#F11B1B" />
+              </BarChart>
+            </ResponsiveContainer>
+
+          </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>xs=12 sm=6</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          <Paper className={classes.paper}>
+            
+          </Paper>
         </Grid>
       </Grid>
+      </Container>
     </div>
     )
   }
